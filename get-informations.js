ea1bec39e2851a2432cd8da3d569ee92f344747e
Cédric JUNG <cedced19@gmail.com>
2022-08-13 16:15:46 +0200

    Update data

---
+++ get-informations.js
@@ -0,0 +1,20 @@
+import { readFile } from 'fs/promises';
+const config = JSON.parse(
+  await readFile(
+    new URL('./config.json', import.meta.url)
+  )
+);
+
+import request from 'then-request';
+
+function req(cb) {
+  request('GET', 'http://' + config.ip_adress + '/status').done(function (res) {
+    let data = JSON.parse(res.getBody('utf8')); 
+    cb(null,{time: data.unixtime, emeters: data.emeters});
+  });
+}
+
+
+req(function (err, result) {
+  console.log(result)
+})
