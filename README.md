8188d9daffa1659077e243cc4d09401b7157eefe
Cédric JUNG <cedced19@gmail.com>
2022-08-14 21:52:18 +0200

    Add files for prod

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # solar-panel-watch
 Watch over electricity consumption from my house.
+
+
+## TODO 
+
+* Add [Rickshaw](https://www.influxdata.com/blog/visualizing-your-time-series-data-from-influxdb-with-rickshaw/)
--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 
 
 const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const logger = require('morgan');
+const compress = require('compression');
+const minifyTemplate = require('express-beautify').minify;
+
 const app = express();
-const port = 8889
+
+var port = require('env-port')('8889');
+app.set('port', port);
+
 const getInformations = require('./get-informations.js')
 
+const i18n = require('./i18n');
+
+app.use(favicon(path.join(__dirname, 'assets','favicon.ico')));
+
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(i18n.init);
+
+if (app.get('env') === 'development') {
+    app.use(logger('dev'));
+} else {
+    app.use(compress());
+    app.use(minifyTemplate());
+}
+
 app.use('/assets/', express.static('assets'));
 
 app.get('/', function(req, res) {
@@ -20,6 +43,7 @@ app.get('/', function(req, res) {
 
 app.use(function (req, res, next) {
     var err = new Error('Element cannot be find.');
+    err.status = 404;
     res.status(404);
     next(err);
 });
@@ -51,5 +75,5 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(port, () => {
-  console.log(`Solar panel watch launched on port ${port}`)
-})
+    console.log(require('server-welcome')(port, 'Solar panel watch'));
+});
