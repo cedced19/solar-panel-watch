0971c2e1a24fb3a1d98e5490ee03cb4c19b01425
Cédric JUNG <cedced19@gmail.com>
2022-08-14 16:48:37 +0200

    Add error handling

--- main.js
+++ main.js
@@ -7,15 +7,49 @@ import getInformations from './get-informations.js'
 
 app.set('view engine', 'ejs');
 app.use('/assets/', express.static('assets'));
+
 app.get('/', function(req, res) {
     getInformations(function (err, data) {
+        if (err) return next(err);
         res.render('index', {
             power1: data.emeters[0].power,
             power2: data.emeters[1].power
         });
-    })
-
+    });
 });
+
+app.use(function (req, res, next) {
+    var err = new Error('Element cannot be find.');
+    res.status(404);
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            status: err.status || 500,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        status: err.status || 500, 
+        error: {}
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Solar panel watch launched on port ${port}`)
