752912def2be1c62deae0ae0792ca04feb829061
Cédric JUNG <cedced19@gmail.com>
2022-10-13 20:53:13 +0200

    Add a threshold

--- get-informations.js
+++ get-informations.js
@@ -2,11 +2,21 @@ const config = require('./config.json');
 
 const request = require('then-request');
 
+let save = {}; 
+let last_save_time = 0;
+
 function req(cb) {
-  request('GET', 'http://' + config.ip_adress_shelly + '/status').done(function (res) {
-    let data = JSON.parse(res.getBody('utf8')); 
-    cb(null,{time: data.unixtime, emeters: data.emeters});
-  });
+  req_time = (new Date()).getTime();
+  if (Math.abs(last_save_time - req_time) > 5000) { // threshold of 5s
+    request('GET', 'http://' + config.ip_adress_shelly + '/status').done(function (res) {
+      let data = JSON.parse(res.getBody('utf8'));
+      save = {time: data.unixtime, emeters: data.emeters};
+      cb(null,save);
+      last_save_time = req_time;
+    });
+  } else {
+    cb(null,save);
+  }
 }
 
 module.exports = req;
