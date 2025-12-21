083864e900128f19378217d3980ca3a03487f82d
Cédric JUNG <cedced19@gmail.com>
2025-12-21 23:07:23 +0100

    Request weather on client side

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const getAlpha = require('./lib/get-alpha.js');
 const influxLib = require('./lib/influx-lib.js');
 const getDeviceEnergy = require('./lib/compute-energy-device.js');
 const computeEnergy = require('./lib/compute-energy.js');
-const getWeather = require('./lib/get-weather.js');
 
 
 const JSONStore = require('json-store-list');
@@ -290,7 +289,12 @@ app.get('/energy-hist/', function(req, res) {
 
 app.get('/energy-graph/', function(req, res) {
     res.render('energy-graph', {
-        timezone: config.timezone
+        timezone: config.timezone,
+        config: {
+            latitude: config.latitude || '',
+            longitude: config.longitude || '',
+            timezone: config.timezone || 'auto'
+        }
     });
 });
 
@@ -366,37 +370,6 @@ app.get('/api/data/energy-request-hist/', (req, res) => {
     });
 });
 
-app.get('/api/data/weather-hist/', (req, res, next) => {
-    // Get date range from query params or use default (last 30 days)
-    let endDate = new Date();
-    let startDate = new Date();
-    
-    if (req.query.start_date && req.query.end_date) {
-        startDate = new Date(req.query.start_date);
-        endDate = new Date(req.query.end_date);
-    } else {
-        // Default to last 30 days
-        startDate.setDate(startDate.getDate() - 30);
-    }
-    
-    getWeather.getHistoricalWeather(startDate, endDate, function (err, data) {
-        if (err) {
-            err.status = 500;
-            res.status(500);
-            return next(err);
-        }
-        // Transform to match expected format
-        const formattedData = data.map(item => ({
-            date: item.date,
-            result: {
-                max_temp: item.max_temp,
-                min_temp: item.min_temp,
-                sunshine_duration: item.sunshine_duration
-            }
-        }));
-        res.json(formattedData);
-    });
-});
 
 app.get('/api/data/power/:tag/:period/group-by/:group/', (req, res, next) => {
     influxLib.requestPowerOverPeriodGroupBy(req.params.period, req.params.tag, req.params.group).then(function (data) {
